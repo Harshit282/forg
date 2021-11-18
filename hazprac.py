cdb13805b4c32a98f08920f1ddbe45e2a50d1eea
Lorem <notloremipsum@protonmail.com>
2021-11-18 12:09:30 +0530

    Minor changes to layout aspect ratio
     Also fix untracked files

--- hazprac.py
+++ hazprac.py
@@ -8,11 +8,9 @@ import os
 class Window(QWidget):
     def __init__(self):
         super().__init__()
-        self.setGeometry(100, 100, 500, 500)
-        self.setWindowTitle('HAZEL')
+        self.setGeometry(100, 100, 1000, 500)
+        self.setWindowTitle('File Organizer')
         main_window_vbox = QVBoxLayout()
-        # vbox = QVBoxLayout()     can be used later if needed....
-        hbox = QHBoxLayout()
         icon1_hbox = QHBoxLayout()
         icon2_hbox = QHBoxLayout()
         icon3_hbox = QHBoxLayout()
@@ -37,29 +35,28 @@ class Window(QWidget):
         # Panel 1 starts from here...
 
         icon1_hbox.addWidget(btn1)
-        icon1_hbox.addStretch(stretch=1)
+        icon1_hbox.addStretch()
         folders_label = QLabel("Folders")
-        listbox = QListWidget()
-        listbox.setMinimumHeight(20)
+        listbox1 = QListWidget()
         panel1_vbox.addLayout(icon1_hbox)
         panel1_vbox.addWidget(folders_label)
-        panel1_vbox.addWidget(listbox)
+        panel1_vbox.addWidget(listbox1)
 
         # Panel 2 starts from here...
 
         icon2_hbox.addWidget(btn2)
-        icon2_hbox.addStretch(stretch=1)
+        icon2_hbox.addStretch()
         panel2_vbox.addLayout(icon2_hbox)
 
         rules_label = QLabel("Rules")
         panel2_vbox.addWidget(rules_label)
 
-        chk_vbox.addSpacerItem(QSpacerItem(150, 350))
-        panel2_vbox.addLayout(chk_vbox)
+        listbox2 = QListWidget()
+        panel2_vbox.addWidget(listbox2)
 
         # Panel 3 starts from here...
 
-        icon3_hbox.addStretch(stretch=1)
+        icon3_hbox.addStretch()
         icon3_hbox.addWidget(btn3)
         panel3_vbox.addLayout(icon3_hbox)
 
@@ -69,26 +66,19 @@ class Window(QWidget):
         panel3_hbox.addWidget(line_edit)
         panel3_vbox.addLayout(panel3_hbox)
 
-        listbox1 = QListWidget()
-        listbox1.insertItem(0, "red")
-        listbox1.insertItem(2, "blue")
-        listbox1.insertItem(0, "green")
-        listbox1.insertItem(0, "yellow")
-        listbox1.setMinimumWidth(900)
-        panel3_vbox.addWidget(listbox1)
+        listbox3 = QListWidget()
+        panel3_vbox.addWidget(listbox3)
 
         # bottom buttons...
-
         save_btn = QPushButton("Save")
         discard_btn = QPushButton("Discard")
-        btm_hbox.addStretch(stretch=1)
+        btm_hbox.addStretch()
         btm_hbox.addWidget(save_btn)
         btm_hbox.addWidget(discard_btn)
-
         # Buttons/Icons clicked actions are defined here...
         # They have been imported from buttons python file...
         def update_folder_list():
-            listbox.insertItem(0, buttons.selected_folders)
+            listbox1.insertItem(0, buttons.selected_folders)
 
         btn1.clicked.connect(buttons.add_folder_clicked)
         btn1.clicked.connect(update_folder_list)
@@ -109,14 +99,18 @@ class Window(QWidget):
                     print(os.path.join(subdir, file), "Size of file is: ", os.path.getsize(os.path.join(subdir, file)),
                           "Bytes")
 
-        listbox.itemClicked.connect(selectionChanged)
+        listbox1.itemClicked.connect(selectionChanged)
 
         # Packing layouts into the main window which is in vertical layout...
 
         all_panel_hbox.addLayout(panel1_vbox)
         all_panel_hbox.addLayout(panel2_vbox)
         all_panel_hbox.addLayout(panel3_vbox)
-        main_window_vbox.addLayout(hbox)
+        # Stretch factor of 1,1,2 leads to 25%, 25%, 50% used space
+        # for panel 1,2,3 respectively
+        all_panel_hbox.setStretchFactor(panel1_vbox,1)
+        all_panel_hbox.setStretchFactor(panel2_vbox,1)
+        all_panel_hbox.setStretchFactor(panel3_vbox,2)
         main_window_vbox.addLayout(all_panel_hbox)
         main_window_vbox.addLayout(btm_hbox)
 
